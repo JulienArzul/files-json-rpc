9ba2c47e43e2a16a1a77ac736610d24c0cabc1cc
Julien Arzul <julien.arzul@gmail.com>
2025-07-26 12:00:29 +0200

    Added basic methods to CRUD a file

--- src/main/kotlin/com/julienarzul/files_json_rpc/service/DefaultFilesRpcService.kt
+++ src/main/kotlin/com/julienarzul/files_json_rpc/service/DefaultFilesRpcService.kt
@@ -3,6 +3,13 @@ package com.julienarzul.files_json_rpc.service
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import java.nio.file.Paths
+import kotlin.io.path.appendText
+import kotlin.io.path.bufferedReader
+import kotlin.io.path.createFile
+import kotlin.io.path.deleteExisting
+import kotlin.io.path.exists
+import kotlin.io.path.pathString
 
 @Service
 class DefaultFilesRpcService : FilesRpcService {
@@ -11,7 +18,51 @@ class DefaultFilesRpcService : FilesRpcService {
         val log: Logger = LoggerFactory.getLogger("DefaultFilesRpcService")
     }
 
+    override fun createFile(filePath: String): String {
+        val path = Paths.get(filePath)
+
+        require(!path.exists()) {
+            "A file or folder already exists at path $filePath"
+        }
+
+        val createdPath = path.createFile()
+
+        log.info("Created file at path $filePath")
+
+        return createdPath.pathString
+    }
+
     override fun appendToFile(filePath: String, textToAppend: String) {
-        log.info("appendToFile called with params: filePath = $filePath, textToAppend = $textToAppend")
+        val path = Paths.get(filePath)
+
+        require(path.exists()) {
+            "No file at path $filePath"
+        }
+
+        path.appendText(textToAppend)
+
+        log.info("Appended $textToAppend to file at path $filePath")
+    }
+
+    override fun readFile(filePath: String): String {
+        val path = Paths.get(filePath)
+
+        require(path.exists()) {
+            "No file at path $filePath"
+        }
+
+        return path.bufferedReader().use { it.readText() }
+    }
+
+    override fun deleteFile(filePath: String) {
+        val path = Paths.get(filePath)
+
+        require(path.exists()) {
+            "No file at path $filePath"
+        }
+
+        path.deleteExisting()
+
+        log.info("Deleted file at path $filePath")
     }
 }
--- src/main/kotlin/com/julienarzul/files_json_rpc/service/FilesRpcService.kt
+++ src/main/kotlin/com/julienarzul/files_json_rpc/service/FilesRpcService.kt
@@ -1,5 +1,11 @@
 package com.julienarzul.files_json_rpc.service
 
 interface FilesRpcService {
+    fun createFile(filePath: String): String
+
     fun appendToFile(filePath: String, textToAppend: String)
+
+    fun readFile(filePath: String): String
+
+    fun deleteFile(filePath: String)
 }
