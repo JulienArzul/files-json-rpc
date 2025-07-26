dd6b047806d04d0fdb59ddef8048d254538c7417
Julien Arzul <julien.arzul@gmail.com>
2025-07-26 09:56:22 +0200

    Initial commit with Spring Boot template

---
+++ src/main/kotlin/com/julienarzul/files_json_rpc/FilesJsonRpcApplication.kt
@@ -0,0 +1,11 @@
+package com.julienarzul.files_json_rpc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FilesJsonRpcApplication
+
+fun main(args: Array<String>) {
+	runApplication<FilesJsonRpcApplication>(*args)
+}
---
+++ src/test/kotlin/com/julienarzul/files_json_rpc/FilesJsonRpcApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.julienarzul.files_json_rpc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FilesJsonRpcApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
